cd595a33d857d47099eff58274e935a60ed03a78
OzaRu <aruberuto03@gmail.com>
2023-10-02 17:49:27 +0200

    ej1

---
+++ EJ1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int num;
+
+    printf("Introduce un numero para comparar respecto al 5: ");
+    scanf("%d", &num);
+
+    if(num > 5)
+        printf("El numero introducido es mayor que 5");
+    
+    else if(num < 5)
+        printf("El numero introducido es menor que 5");
+    
+    else
+        printf("El numero introducido es 5");
+        
+    return 0;
+}
