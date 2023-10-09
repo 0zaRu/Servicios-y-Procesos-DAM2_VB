64238409e5047815869b776edb3a7a62e2ba688b
OzaRu <aruberuto03@gmail.com>
2023-10-09 16:28:04 +0200

    commit

--- Primeros ejercicios/EJ10.c
+++ Primeros ejercicios/EJ10.c
@@ -1,38 +1,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#define MAX 25
-#define MIN 10
+#define MAX 10
+#define MIN 2
 
 int pideEnteroRangValor(int, int);
-void mostrarArray(int[]);
-void rellenaArrayRand(int*);
 
 int main(void)
 {
-    int tam1, tam2;
+    int tam1, tam2, tam3;
     bool correcto = false;
 
-    printf("Introduce el tamano del primer array que vas a multiplicar: ");
+    printf("Introduce el largo de la primera matriz: ");
     tam1 = pideEnteroRangValor(MIN, MAX);
 
-    printf("Introduce el tamano del segundo array que vas a multiplicar: ");
+    printf("Introduce el ancho de la primera matriz: ");
     tam2 = pideEnteroRangValor(MIN, MAX);
 
-    int v1[tam1], v2[tam2], vResult[tam1];
+    printf("Introduce el ancho de la segunda matriz: ");
+    tam3 = pideEnteroRangValor(MIN, MAX);
 
-    rellenaArrayRand(v1);
-    mostrarArray(v1);
-    rellenaArrayRand(v2);
-    mostrarArray(v2);
+    int v1[tam1][tam2], v2[tam2][tam3], vResult[tam1][tam3];
 
-    printf("\n\n\n");
+    for(int i = 0; i < tam1; i++)
+        for(int j = 0; j < tam2; j++){
+            v1[i][j] = rand()%10;
 
-    for(int i = 0; i<tam1; i++)
-        for(int j = 0; j<tam2; j++)
-            vResult[i] += v1[i] * v2[j];
+            for(int k = 0; k < tam3; k++)
+                v2[j][k] = rand()%10;
+        }
 
-    mostrarArray(vResult);
+    printf("\n\nLa primera matriz: \n================================\n");
+    for(int i=0; i<tam1; i++){
+        for(int j=0; j < tam2; j++)
+            printf("  %d", v1[i][j]);
+        printf("\n");
+    }
+
+    printf("\n\nLa segunda matriz: \n================================\n");
+    for(int i=0; i<tam2; i++){
+        for(int j=0; j < tam3; j++)
+            printf("  %d", v2[i][j]);
+        printf("\n");
+    }
+
+
+    for(int i = 0; i < tam1; i++)
+        for(int j = 0; j < tam3; j++){
+            vResult[i][j] = 0;
+        
+            for(int k = 0; k < tam2; k++)
+                vResult[i][j] += v1[i][k] * v2[k][j];
+        }
+    printf("\n\n\nLa matriz resultante: \n================================\n");
+    for(int i=0; i<tam1; i++){
+        for(int j=0; j < tam3; j++)
+            printf("  %d", vResult[i][j]);
+        printf("\n");
+    }
 
     return 0;
 }
