7e05df60392535b312bda200a0e74f0f1f4141bc
0zaru <aruberuto03@gmail.com>
2023-10-19 16:57:19 +0200

    commit

--- Tema 1/EJ5_1.c
+++ Tema 1/EJ5_1.c
@@ -1,3 +1,5 @@
+//Ejercicio 5.1 de Alberto Rodríguez
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -7,20 +9,21 @@ int main() {
     pid_t pid_hijo, pid_nieto;
 
     pid_hijo = fork();
+    valor -= 2;
+
     if (pid_hijo == 0) {
         // Este es el proceso hijo
-        valor -= 2;
         printf("Hijo: El valor es %d\n", valor);
         
         pid_nieto = fork();
+        valor -= 2;
+
         if (pid_nieto == 0) {
             // Este es el proceso nieto
-            valor -= 2;
             printf("Nieto: El valor es %d\n", valor);
         }
     } else {
         // Este es el proceso padre
-        valor -= 2;
         printf("Padre: El valor es %d\n", valor);
     }
 
