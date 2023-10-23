2ae14e6654ded0895d7a22f4409c93f0065b3bc9
0zaru <aruberuto03@gmail.com>
2023-10-23 17:01:44 +0200

    commit

--- Tema 1/EJ9.c
+++ Tema 1/EJ9.c
@@ -4,19 +4,19 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-int instanciaProceso(pid_t proceso, int nHijo);
+int instanciaProceso(pid_t proceso, int nHijo, int tab);
 
 int main (void){
     
-    printf("Soy el proceso 1, mi PID es %d y el PID de mi padre es %d\n", getpid(), getppid());
+    printf("\tSoy el proceso 1, mi PID es %d y el PID de mi padre es %d\n", getpid(), getppid());
     
-    if(instanciaProceso(fork(), 2)){
-        !instanciaProceso(fork(), 5) && !instanciaProceso(fork(), 6) && !instanciaProceso(fork(), 7);
+    if(instanciaProceso(fork(), 2, 2)){
+        !instanciaProceso(fork(), 5, 3) && !instanciaProceso(fork(), 6, 3) && !instanciaProceso(fork(), 7, 3);
 
     }else{
-        if(!instanciaProceso(fork(), 3) && instanciaProceso(fork(), 4))
-            if(instanciaProceso(fork(), 8))
-                instanciaProceso(fork(), 9);
+        if(!instanciaProceso(fork(), 3, 2) && instanciaProceso(fork(), 4, 2))
+            if(instanciaProceso(fork(), 8, 3))
+                instanciaProceso(fork(), 9, 4);
     }
 
     while(wait(NULL) != -1);
@@ -26,16 +26,20 @@ int main (void){
 }
 
 
-int instanciaProceso(pid_t proc, int nH){
+int instanciaProceso(pid_t proc, int nH, int tab){
 
     switch(proc){
         case -1:
             printf("Error durante la creacion\n");
             break;
         case 0:     //Proceso hijo
+
+            for(int i = 0; i < tab; i++)
+                printf("\t");
+
             printf("Soy el proceso %d, mi PID es %d y el PID de mi padre es %d\n", nH, getpid(), getppid());
             return 1;
-        default:
+        default:    //Proceso padre
             return 0;
     }
 }
