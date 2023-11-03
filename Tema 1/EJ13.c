8eea54f00d694d7f7862a41eae3fa21a808cac07
0zaru <aruberuto03@gmail.com>
2023-11-03 03:13:35 +0100

    EJ13

--- Tema 1/EJ13.c
+++ Tema 1/EJ13.c
@@ -1,7 +1,5 @@
 //Codigo de Alberto Rodríguez
 
-//Se utuliza true si es el proceso hijo y false si es el proceso padre
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,91 +9,87 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int instanciaProceso(int *nPid, int fd[]);
-void escribePipe(int fd[], int numero);
-int leePipe(int fd[]);
+int creaLos9Procesos(pid_t p[]);
+int instanciaProceso(int *nPid);
 
 int main (void){
+    system("rm FIFO");
 
-    pid_t p1 = getpid(), p2, p3, p4, p5, p6, p7, p8, p9;
-    int numero=0;
+    pid_t p[10];
+    int numero = creaLos9Procesos(p);
 
-    if(mknod("./FIFO", __S_IFIFO | 0666, 0) == -1){
-        
-    }
+    if(getpid() == p[1]){
+        int fp, bytesLeidos;
+        char buffer[1] = "";
 
-    if(instanciaProceso(&p2)){                                   //A PARTIR DE AQUÍ ES SOLO PROCESO 2
-        !instanciaProceso(&p5) && !instanciaProceso(&p6) && !instanciaProceso(&p7);
+        if(mknod("FIFO", __S_IFIFO | 0666, 0) == -1){
+            printf("HA OCURRIDO UN ERROR ...\n");
+            exit(-1);
+        }
 
-    
-    }else{                                                              //A PARTIR DE AQUÍ ES SOLO PROCESO 1
-        if(!instanciaProceso(&p3) && instanciaProceso(&p4))             //A PARTIR DE AQUÍ ES SOLO PROCESO 4
-            if(instanciaProceso(&p8))                                   //A PARTIR DE AQUÍ ES SOLO PROCESO 8
-                instanciaProceso(&p9)                                   //A PARTIR DE AQUÍ ES SOLO PROCESO 9
-    }
+        while(1){
+            fp = open("FIFO", 0);
+            bytesLeidos = read(fp, buffer, sizeof(buffer));
+            
+            while(bytesLeidos != 0){
+                //printf("%s\n", buffer); 
+                numero += atoi(buffer);
+                bytesLeidos = read(fp, buffer, sizeof(buffer));
 
-    if(getpid() == p1){
-        numero = 5;
+                printf("Proceso 1 tiene el valor (43): %d.\n", numero);
+            }
+            close(fp);
+        }
+    }else{
 
-        printf("El proceso 1 termina con el valor %d\n", numero);
-    }else if(getpid() == p2){
-        numero = 5;
-         
-        printf("El proceso 2 termina con el valor %d\n", numero);
-    }else if(getpid() == p3){
-        numero = 7;
-    
-        printf("El proceso 3 termina con el valor %d\n", numero);
-    }else if(getpid() == p4){
-        numero = 3;
+        int fp;
+        char numACad[5];
+        sprintf(numACad, "%d", numero);
 
-        printf("El proceso 4 termina con el valor %d\n", numero);
-    }else if(getpid() == p5){
-        numero = 1;
-        
-        printf("El proceso 5 termina con el valor %d\n", numero);
-    }else if(getpid() == p6){
-        numero = 8;
+        sleep(1);
+        fp = open("FIFO", 1);
 
-        printf("El proceso 6 termina con el valor %d\n", numero);
-    }else if(getpid() == p7){
-        numero = 2;
+        if(fp == -1){
+            printf("ERROR AL ABRIR EL FICHERO ...\n");
+            exit(-1);
+        }
+        //printf("Envio el %d\n", numero);
+        write(fp, numACad, strlen(numACad));
+        close(fp);
 
-        printf("El proceso 7 termina con el valor %d\n", numero);
-    }else if(getpid() == p8){
-        numero = 3;
-
-        printf("El proceso 8 termina con el valor %d\n", numero);
-    }else if(getpid() == p9){
-        numero = 9;
-
-        printf("El proceso 9 termina con el valor %d\n", numero);
-    }
+    } 
 
     while(wait(NULL) != -1);
     return 0;
 }
 
-void escribePipe(int fd[], int numero){
-    char numACad[10];
-    sprintf(numACad, "%d", numero);
-    
-    write(fd[1], numACad, strlen(numACad));
-    close(fd[1]);
+
+int creaLos9Procesos(pid_t p[]){
+    p[1] = getpid();
+
+    if(instanciaProceso(&p[2])) {                                       //A PARTIR DE AQUÍ ES SOLO PROCESO 2
+        !instanciaProceso(&p[5]) && !instanciaProceso(&p[6]) && !instanciaProceso(&p[7]);
+
+    }else{                                                              //A PARTIR DE AQUÍ ES SOLO PROCESO 1
+        if(!instanciaProceso(&p[3]) && instanciaProceso(&p[4]))         //A PARTIR DE AQUÍ ES SOLO PROCESO 4
+            if(instanciaProceso(&p[8]))                                 //A PARTIR DE AQUÍ ES SOLO PROCESO 8
+                instanciaProceso(&p[9]);                                //A PARTIR DE AQUÍ ES SOLO PROCESO 9
+    }
+
+    //######################################################################################################
+
+    if     (getpid() == p[1]) return 5;
+    else if(getpid() == p[2]) return 5;
+    else if(getpid() == p[3]) return 7;
+    else if(getpid() == p[4]) return 3;
+    else if(getpid() == p[5]) return 1;
+    else if(getpid() == p[6]) return 8;
+    else if(getpid() == p[7]) return 2;
+    else if(getpid() == p[8]) return 3;
+    else if(getpid() == p[9]) return 9;
 }
 
-int leePipe(int fd[]){
-    char buffer[10];
-    memset(buffer, 0, sizeof(buffer));
-
-    read(fd[0], buffer, sizeof(buffer));
-    close(fd[0]);
-
-    return atoi(buffer);
-}
-
-int instanciaProceso(int *nPid, int fd[]){
-    pipe(fd);
+int instanciaProceso(int *nPid){
 
     switch (fork()) {
         case -1:
@@ -103,12 +97,10 @@ int instanciaProceso(int *nPid, int fd[]){
             return -1;
 
         case 0:  // Proceso hijo
-            close(fd[0]);
             *nPid = getpid();
             return 1;
 
         default:  // Proceso padre
-            close(fd[1]);
             return 0;
     }
 }
